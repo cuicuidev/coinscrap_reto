fd7b5cfe6dd69a9c6a2edd06505a15280d6d22b0
Dmitry Ryzhenkov <dmitryryzhenkov.dev@gmail.com>
2023-11-30 00:18:17 +0100

    some evaluation methods idk

--- eval.py
+++ eval.py
@@ -8,7 +8,8 @@ class Eval:
     def __init__(self, df: pd.DataFrame | None = None, feature_importances: dict | None = None):
 
         self.df = df
-        if self.df is None:
+        if df is None:
+            print('heeheee')
             self.df = pd.concat([pd.read_csv(csv, sep=";") for csv in glob.glob("*.csv")])
 
         self.feature_importances = {"curtosis" : 1, "symetry" : 1, "time_series_continuity" : 1}
@@ -17,7 +18,43 @@ class Eval:
 
         self.eval = None
 
-        self.population_eval = self._get_metrics()
+        self.population_eval = self._get_metrics(self.df)
+
+        def process_df(df: pd.DataFrame) -> pd.DataFrame:
+            df['datetime'] = pd.to_datetime(df.date)
+
+            df['year'] = df.datetime.dt.year
+            df['month'] = df.datetime.dt.month
+            df['day'] = df.datetime.dt.day
+            df['hour'] = df.datetime.dt.hour
+            df['minute'] = df.datetime.dt.minute
+            df['second'] = df.datetime.dt.second
+            df['utc_offset'] = df.datetime.apply(lambda x: x.utcoffset().total_seconds())
+
+            seasons_map = {
+                1 : 'winter',
+                2 : 'winter',
+                3 : 'spring',
+                4 : 'spring',
+                5 : 'spring',
+                6 : 'summer',
+                7 : 'summer',
+                8 : 'summer',
+                9 : 'fall',
+                10 : 'fall',
+                11 : 'fall',
+                12 : 'winter',
+            }
+            df['season'] = df.month.replace(seasons_map)
+
+            df.rename(columns={"Categoría": "category"}, inplace=True)
+
+            df = df.drop("date", axis=1)
+
+            return df
+
+        self.df = process_df(self.df)
+
 
     def _get_metrics(self, df) -> pd.DataFrame:
         pass
@@ -45,3 +82,9 @@ class Eval:
             evals.append(self._evaluate_sample(n))
         self.eval = pd.concat(evals)
         return self.eval
+
+
+
+if __name__ == '__main__':
+    e = Eval()
+    e.plot()
