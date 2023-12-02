63616174389ed44ff95cf4ecc7b3007f7a7dac63
Dmitry Ryzhenkov <dmitryryzhenkov.dev@gmail.com>
2023-12-02 10:57:16 +0100

    ClusterSampling finished

--- evaluation/sampling.py
+++ evaluation/sampling.py
@@ -1,5 +1,7 @@
 from typing import Optional, Iterable
 import pandas as pd
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import MinMaxScaler, LabelEncoder
 
 class SamplingStrategy:
 
@@ -33,7 +35,6 @@ class StratifiedRandomSampling(SamplingStrategy):
         def take_sample(group: pd.DataFrame) -> pd.DataFrame:
             proportion = len(group) / len(df)
             stratum_sample_size = round(n * proportion)
-            # print(stratum_sample_size)
             return group.sample(min(len(group), stratum_sample_size), random_state=random_state)
         
         sample: pd.DataFrame = df.groupby(self.strata, group_keys=False, as_index=False).apply(take_sample)
@@ -49,10 +50,47 @@ class ClusterSampling(SamplingStrategy):
     What if we cluster the data and use the cluster column as a strata?
     """
 
-    def __init__(self, n_clusters: int, cluster_data: Iterable[str]) -> None:
+    def __init__(self, n_clusters: int, fields: Iterable[str]) -> None:
         self.n_clusters = n_clusters
-        self.cluster_data = cluster_data
+        self.fields = fields
 
     def sample(self, df: pd.DataFrame, n: int, random_state: int | float | None) -> pd.DataFrame:
-        s = df.sample(n=n, random_state=random_state)
-        return s # TODO: implement clustering
+        df = df.copy()
+        kmeans = KMeans(n_clusters=self.n_clusters)
+        scaler = MinMaxScaler()
+        cluster_data = df[self.fields]
+        X, encodings = self._encode(cluster_data)
+        X = scaler.fit_transform(X)
+        kmeans.fit(X)
+        df['cluster'] = kmeans.labels_
+        
+        sample: pd.DataFrame = df.groupby('cluster', group_keys=False, as_index=False).apply(self._sample_group, df_size= len(df), total_sample_size=n, random_state=random_state)
+        if len(sample) < n:
+            remaining_df = df.merge(sample, indicator=True, how='outer').loc[lambda x: x['_merge'] == 'left_only']
+            random_sample = remaining_df.sample(n-len(sample), random_state=42)
+            sample = pd.concat([sample, random_sample]).drop(['_merge', 'cluster'], axis=1)
+
+        # sample = self._decode(sample, encodings)
+
+        return sample
+    
+    def _encode(self, df: pd.DataFrame) -> tuple[pd.DataFrame, dict]:
+        encoded_df = df.copy()
+        encodings = {}
+        for column in encoded_df.columns:
+            if encoded_df[column].dtype == 'object':
+                encoder = LabelEncoder()
+                encoded_df[column] = encoder.fit_transform(encoded_df[column])
+                encodings[column] = encoder
+        return encoded_df, encodings
+    
+    def _decode(self, df: pd.DataFrame, encodings: dict) -> pd.DataFrame:
+        decoded_df = df.copy()
+        for column, encoder in encodings.items():
+            decoded_df[column] = encoder.inverse_transform(decoded_df[column])
+        return decoded_df
+    
+    def _sample_group(self, group: pd.DataFrame, df_size: int, total_sample_size: int, random_state: int | float | None = None) -> pd.DataFrame:
+        proportion = len(group) / df_size
+        stratum_sample_size = round(total_sample_size * proportion)
+        return group.sample(min(len(group), stratum_sample_size), random_state=random_state)
